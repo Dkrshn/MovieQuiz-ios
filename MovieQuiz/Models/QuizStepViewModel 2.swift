36a22f51a64cfad870b7823a5ad8518f662fe72a
Krasheninnikov Daniil <d.krash@icloud.com>
2022-11-20 13:37:42 +0300

    Исправил замечания по заданию 5-го спринта

---
+++ MovieQuiz/Models/QuizStepViewModel 2.swift
@@ -0,0 +1,17 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Даниил Крашенинников on 27.11.2022.
+//
+
+import Foundation
+import UIKit
+
+
+// для состояния "Вопрос задан"
+struct QuizStepViewModel { 
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
---
+++ MovieQuiz/Services/AlertPresenterProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  AlertPresenterProtocol.swift
+//  MovieQuiz
+//
+//  Created by Даниил Крашенинников on 30.11.2022.
+//
+
+import Foundation
+import UIKit
+
+protocol AlertPresenterProtocol { 
+    
+    func showAlert(result: AlertModel)
+    
+}
